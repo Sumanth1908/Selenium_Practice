661faa40be4f78f8858475a1fef4879fe24ac585
Sumanth1908 <jschary555@gmail.com>
2018-06-13 23:42:05 +0530

    updated pom file and created a new class

---
+++ Selenium/src/test/java/Sumanth/Selenium/faceBookTest.java
@@ -0,0 +1,8 @@
+package Sumanth.Selenium;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class faceBookTest {
+	WebDriver driver=new ChromeDriver();
+	
+	
+}
